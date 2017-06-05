54a23d28f816f70a58133654b45e00e00e2737cc
Dane Petersen <thegreatsunra@gmail.com>
2017-06-05 17:50:11 -0500

    Init new repo with azure iot hub node example

---
+++ createdeviceidentity/CreateDeviceIdentity.js
@@ -0,0 +1,23 @@
+const iothub = require('azure-iothub')
+
+const connectionString = '{your_iothub_connection_string}'
+
+const registry = iothub.Registry.fromConnectionString(connectionString)
+
+let device = new iothub.Device(null)
+device.deviceId = 'myFirstNodeDevice'
+registry.create(device, (err, deviceInfo, res) => {
+  if (err) {
+    registry.get(device.deviceId, printDeviceInfo)
+  }
+  if (deviceInfo) {
+    printDeviceInfo(err, deviceInfo, res)
+  }
+})
+
+function printDeviceInfo(err, deviceInfo, res) {
+  if (deviceInfo) {
+    console.log('Device ID: ' + deviceInfo.deviceId)
+    console.log('Device key: ' + deviceInfo.authentication.symmetricKey.primaryKey)
+  }
+}
